cd44f2226a4edb59dc9a6ca8d3f4493602e8430c
Arnaud Delaunoy <arnaud.delaunoy@student.uliege.be>
2018-05-05 19:06:12 +0200

    adding convergeance cirteria

--- test_metro.m
+++ test_metro.m
@@ -1,7 +1,7 @@
 function test_metro
 
 start_state = 1:40;
-mar_chaine = Metro_Hast(start_state, 10000);
+[nbIterations, mar_chaine] = Metro_Hast(start_state, 120, 20000);
 
 result = mar_chaine(end, :);
 
@@ -11,5 +11,6 @@ D_int = arrayfun(@(x) symbol_to_int(x), D(1:(end -1)));
 decoded = decode(D_int, result);
 
 seq = arrayfun(@(x) int_to_symbol(x), decoded)
+nbIterations
 
 end
